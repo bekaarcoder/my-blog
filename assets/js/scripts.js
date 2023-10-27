f5be43ad85dfca34b8262863352e889f90cd480a
Shashank <s_shashank@outlook.com>
2023-10-27 20:28:27 +0530

    persisting current theme during browser session

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,25 @@
 const htmlElement = document.querySelector('html');
 const themeToggleBtn = document.querySelector('#theme-toggle');
 
+const currentTheme = htmlElement.getAttribute('data-bs-theme');
+if (sessionStorage.getItem('currentTheme')) {
+    htmlElement.setAttribute(
+        'data-bs-theme',
+        sessionStorage.getItem('currentTheme')
+    );
+} else {
+    sessionStorage.setItem('currentTheme', currentTheme);
+}
+
 function toggleTheme() {
-    const currentTheme = htmlElement.getAttribute('data-bs-theme');
-    console.log('Current Theme: ', currentTheme);
-    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    const newTheme =
+        sessionStorage.getItem('currentTheme') === 'dark' ? 'light' : 'dark';
     if (newTheme === 'dark') {
         themeToggleBtn.className = 'bi bi-brightness-high';
     } else {
         themeToggleBtn.className = 'bi bi-brightness-high-fill';
     }
     htmlElement.setAttribute('data-bs-theme', newTheme);
+    // sessionStorage.removeItem("currentTheme")
+    sessionStorage.setItem('currentTheme', newTheme);
 }
