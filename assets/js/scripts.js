1f784f613beed1f672e2b7af7ee4bec0ad6f2a2e
bekaarcoder <bekaarcoder@users.noreply.github.com>
2024-12-06 18:38:46 +0000

    jekyll build from Action 164e54c4d03381e19ac649537dc341ddb69120cc

---
+++ assets/js/scripts.js
@@ -0,0 +1,41 @@
+const htmlElement = document.querySelector('html');
+const themeToggleBtn = document.querySelector('#theme-toggle');
+const prevBtn = document.querySelector('.previous');
+const nextBtn = document.querySelector('.next');
+
+const currentTheme = htmlElement.getAttribute('data-bs-theme');
+if (sessionStorage.getItem('currentTheme')) {
+    htmlElement.setAttribute(
+        'data-bs-theme',
+        sessionStorage.getItem('currentTheme')
+    );
+} else {
+    sessionStorage.setItem('currentTheme', currentTheme);
+}
+
+if (sessionStorage.getItem('currentTheme') === 'dark') {
+    themeToggleBtn.className = 'bi bi-moon-stars-fill text-warning';
+    prevBtn && (prevBtn.className = 'previous btn btn-light');
+    nextBtn && (nextBtn.className = 'next btn btn-light ms-auto');
+} else {
+    themeToggleBtn.className = 'bi bi-sun-fill text-warning';
+    prevBtn && (prevBtn.className = 'previous btn btn-dark');
+    nextBtn && (nextBtn.className = 'next btn btn-dark ms-auto');
+}
+
+function toggleTheme() {
+    const newTheme =
+        sessionStorage.getItem('currentTheme') === 'dark' ? 'light' : 'dark';
+    if (newTheme === 'dark') {
+        themeToggleBtn.className = 'bi bi-moon-stars-fill text-warning';
+        prevBtn && (prevBtn.className = 'previous btn btn-light');
+        nextBtn && (nextBtn.className = 'next btn btn-light ms-auto');
+    } else {
+        themeToggleBtn.className = 'bi bi-sun-fill text-warning';
+        prevBtn && (prevBtn.className = 'previous btn btn-dark');
+        nextBtn && (nextBtn.className = 'next btn btn-dark ms-auto');
+    }
+    htmlElement.setAttribute('data-bs-theme', newTheme);
+    // sessionStorage.removeItem("currentTheme")
+    sessionStorage.setItem('currentTheme', newTheme);
+}
